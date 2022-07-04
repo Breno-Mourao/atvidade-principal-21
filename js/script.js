aaeea480c754b862535c07049cc0afaf0ac5d3c2
Breno-Mourao <breno.silva18@gmail.com>
2022-07-04 16:11:16 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function post() {
+  // delcaração de variáveis
+  var nome = document.getElementById('nome').value;
+  var comentario = document.getElementById('comentario').value;
+  var corDeTexto = document.getElementById('cor-texto').value;
+  var corDeFundo = document.getElementById('cor-fundo').value;
+  var post = document.getElementById('post');
+
+  // output post
+  post.innerHTML += '<div style="color: '+ corDeTexto + '; background-color: ' + corDeFundo + ';"><p id="nome-post">' + nome + '<hr></p><p id="comentario-post">' + comentario + '</p></div>';
+}
